f9f6ea08809b5c9189112d30bb96d5871a2eb989
Agustín Cisneros <agustincc@tutanota.com>
2025-10-06 14:54:40 +0200

    use plain style for almost all required details

--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,7 @@
 { config, pkgs, ... }:
 {
-  home = {
-    username = "aerz";
-    stateVersion = "25.05";
-  };
+  home.username = "aerz";
+  home.stateVersion = "25.05";
 
   imports = [
     ./kitty.nix
