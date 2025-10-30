76810311218de8d875b179dd464f6e5b178d7816
Agustín Cisneros <agustincc@tutanota.com>
2025-10-30 22:18:23 +0100

    init fish config

--- home/fish.nix
+++ home/fish.nix
@@ -5,5 +5,67 @@
   ...
 }:
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+
+    plugins = [
+      {
+        name = "fish-you-should-use";
+        src = pkgs.fetchFromGitHub {
+          owner = "paysonwallach";
+          repo = "fish-you-should-use";
+          rev = "a332823512c0b51e71516ebb8341db0528c87926";
+          hash = "sha256-MmGDFTgxEFgHdX95OjH3jKsVG1hdwo6bRht+Lvvqe5Y=";
+        };
+      }
+    ];
+
+    interactiveShellInit = ''
+      set fish_greeting
+    '';
+
+    functions = {
+      C = {
+        description = "Copy stdin into clipboard";
+        body = "pbcopy";
+      };
+      print-term-colors = {
+        description = "Show all terminal colors (0-255)";
+        body = ''
+          for i in (seq 0 255)
+            echo (tput setaf $i)"This is color 󱓻 $i"(tput sgr0)
+          end
+        '';
+      };
+    };
+
+    shellAbbrs = {
+      bench-fish = "hyperfine --warmup 5 'fish -i -c exit'";
+      c = "clear";
+    };
+
+    shellAliases = {
+      ".." = "cd ..";
+      g = "git";
+      grep = "grep --color=auto";
+      ls = "eza --icons=always --color=always --group-directories-first";
+      find = "fd";
+      l = "ls -lgh";
+      ll = "ls -l";
+      la = "ls -la";
+      tree = "eza --icons --tree --group-directories-first";
+      cp = "cp -vi";
+      mv = "mv -vi";
+      rm = "rm -vI";
+      duh = "du -sh";
+
+      vim = "nvim";
+      vimdiff = "nvim -d";
+
+      rsync = "rsync -vrPlu";
+      ffmpeg = "ffmpeg -hide_banner";
+
+      sshsum = "ssh-keygen -l -f";
+    };
+  };
 }
