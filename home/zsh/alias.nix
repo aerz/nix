846d01f6492a1e421625fda19e9c51ad8470a50b
Agustín Cisneros <agustincc@tutanota.com>
2025-10-30 11:17:53 +0100

    split aliases to a file

---
+++ home/zsh/alias.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+  programs.zsh = {
+    shellAliases = {
+      ".." = "cd ..";
+      c = "clear";
+      g = "git";
+      grep = "grep --color=auto";
+      ls = "eza --icons=always --color=always --group-directories-first";
+      find = "fd";
+      l = "ls -lgh";
+      ll = "ls -l";
+      la = "ls -la";
+      tree = "eza --icons --tree --group-directories-first";
+      cp = "cp -vi";
+      mv = "mv -vi";
+      rm = "rm -vI";
+      duh = "du -sh";
+
+      bench = "hyperfine";
+      vim = "nvim";
+      vimdiff = "nvim -d";
+
+      rsync = "rsync -vrPlu";
+      ffmpeg = "ffmpeg -hide_banner";
+
+      sshsum = "ssh-keygen -l -f";
+    };
+
+    shellGlobalAliases = {
+      C = "| pbcopy";
+    };
+  };
+}
--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -7,6 +7,7 @@
 {
   imports = [
     ./keybindings.nix
+    ./alias.nix
   ];
 
   home.file.".hushlogin".text = "";
@@ -128,36 +129,6 @@
       saveNoDups = true;
       ignoreDups = true;
       findNoDups = true;
-    };
-
-    shellAliases = {
-      ".." = "cd ..";
-      c = "clear";
-      g = "git";
-      grep = "grep --color=auto";
-      ls = "eza --icons=always --color=always --group-directories-first";
-      find = "fd";
-      l = "ls -lgh";
-      ll = "ls -l";
-      la = "ls -la";
-      tree = "eza --icons --tree --group-directories-first";
-      cp = "cp -vi";
-      mv = "mv -vi";
-      rm = "rm -vI";
-      duh = "du -sh";
-
-      bench = "hyperfine";
-      vim = "nvim";
-      vimdiff = "nvim -d";
-
-      rsync = "rsync -vrPlu";
-      ffmpeg = "ffmpeg -hide_banner";
-
-      sshsum = "ssh-keygen -l -f";
-    };
-
-    shellGlobalAliases = {
-      C = "| pbcopy";
     };
   };
 
