cb61edfe9b211b1d7c0279f9d4d966d501ee3bf5
Agustín Cisneros <agustincc@tutanota.com>
2025-11-05 00:00:47 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     # NOTE: python 3.14 migration issues, homebrew-core pinned temporary
     # https://github.com/Homebrew/homebrew-core/issues/248654
-    homebrew-core.url = "github:homebrew/homebrew-core/0eced82c88d205378ca7963ab9db9e8a76c861ef";
+    homebrew-core.url = "github:homebrew/homebrew-core/f9d29b9579916906c00ac692549cade0b1725c16";
     homebrew-core.flake = false;
     homebrew-cask.url = "github:homebrew/homebrew-cask";
     homebrew-cask.flake = false;
