b257740407ca4c7b8e64916056d7090cec0dc753
Agustín Cisneros <agustincc@tutanota.com>
2025-11-05 22:03:18 +0100

    add astro profile for vscode

--- home/vscode.nix
+++ home/vscode.nix
@@ -10,6 +10,7 @@ let
     "workbench.navigationControl.enabled" = false;
     "workbench.preferredLightColorTheme" = "GitHub Light";
     "workbench.preferredDarkColorTheme" = "GitHub Dark Default";
+    "workbench.iconTheme" = "vscode-jetbrains-icon-theme-2023-auto";
     "workbench.startupEditor" = "none";
     "workbench.activityBar.location" = "hidden";
     "workbench.editor.showTabs" = "none";
@@ -30,12 +31,16 @@ let
 
     "editor.fontLigatures" = true;
     "editor.formatOnSave" = true;
+    "editor.insertSpaces" = true;
+    "editor.tabSize" = 2;
     "editor.rulers" = [
       80
       120
     ];
-    "editor.minimap.enabled" = false;
     "editor.fontFamily" = "'JetBrainsMono Nerd Font', monospace";
+    "editor.minimap.enabled" = false;
+    "editor.lineNumbers" = "relative";
+    "editor.cursorBlinking" = "smooth";
 
     "extensions.showRecommendationsOnlyOnDemand" = true;
     "extensions.ignoreRecommendations" = true;
@@ -44,12 +49,17 @@ let
     "diffEditor.ignoreTrimWhitespace" = false;
 
     "telemetry.telemetryLevel" = "off";
+    "update.mode" = "none";
   };
 
-  default_extensions = with pkgs.vscode-extensions; [
-    github.github-vscode-theme
-  ];
-
+  chadalen.vscode-jetbrains-icon-theme = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "vscode-jetbrains-icon-theme";
+      publisher = "chadalen";
+      version = "2.36.0";
+      hash = "sha256-p5hqytkF5Hg2d9N+XwZ5DfG2GEfoSPYXX0FCeUUR2Yc=";
+    };
+  };
   yanivmo.navi-cheatsheet-language = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     mktplcRef = {
       name = "navi-cheatsheet-language";
@@ -58,6 +68,20 @@ let
       hash = "sha256-xnFnX3sa5kblw+kIoJ5CkrZUHDKaxxjzdn361eY0dKE=";
     };
   };
+  dnut.rewrap-revived = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    mktplcRef = {
+      name = "rewrap-revived";
+      publisher = "dnut";
+      version = "17.9.0";
+      hash = "sha256-au71N3gVDMKnTX9TXzGt9q4b3OM7s8gMHXBnIVZ/1CE=";
+    };
+  };
+
+  default_extensions = with pkgs.vscode-extensions; [
+    github.github-vscode-theme
+    alefragnani.project-manager
+    chadalen.vscode-jetbrains-icon-theme
+  ];
 in
 {
   programs.vscode = {
@@ -86,6 +110,31 @@ in
           "[nix]"."editor.tabSize" = 2;
         } default_user_settings;
       };
+
+      astro = {
+        extensions =
+          with pkgs.vscode-extensions;
+          [
+            astro-build.astro-vscode
+            bradlc.vscode-tailwindcss
+            dnut.rewrap-revived
+            esbenp.prettier-vscode
+            ms-vscode.live-server
+            usernamehw.errorlens
+          ]
+          ++ default_extensions;
+
+        userSettings = lib.mergeAttrs {
+          "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+          "typescript.updateImportsOnFileMove.enabled" = "always";
+          "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+          "javascript.updateImportsOnFileMove.enabled" = "always";
+          "[mdx]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+          "[jsonc]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+          "rewrap.autoWrap.enabled" = true;
+        } default_user_settings;
+      };
+
     };
   };
 }
