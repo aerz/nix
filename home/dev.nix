ba7b24b2dcdf769269970ced0249dc4535949e62
Agustín Cisneros <agustincc@tutanota.com>
2026-01-03 20:20:35 +0100

    add gpg with pinentry-mac

--- home/dev.nix
+++ home/dev.nix
@@ -9,4 +9,14 @@
     enable = true;
     nix-direnv.enable = true;
   };
+
+  programs.gpg = {
+    enable = true;
+    homedir = "${config.xdg.dataHome}/gnupg";
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentry.package = pkgs.pinentry_mac;
+  };
 }
