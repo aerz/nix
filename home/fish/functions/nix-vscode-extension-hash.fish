4a2eeae14addcfb2888358ee5363a0c07e44d15d
Agustín Cisneros <agustincc@tutanota.com>
2026-01-11 18:25:42 +0100

    refactor nix-vscode-extension-hash
    * accept one argument and optional version
    * validate extension id format
    * fetch latest version when unspecified
    * print ready-to-use attribute set

--- home/fish/functions/nix-vscode-extension-hash.fish
+++ home/fish/functions/nix-vscode-extension-hash.fish
@@ -1,25 +1,60 @@
-function nix-vscode-extension-hash -d "Generate hash value for nixpkgs.vscode-util.buildVscodeMarketplaceExtension.hash attribute"
-    argparse --max-args 0 h/help 'a/author=' 'n/name=' 'v/version=' -- $argv
+function nix-vscode-extension-hash -d "Generate sha256 hash for nixpkgs vscode-utils.buildVscodeMarketplaceExtension"
+    argparse --max-args 1 --min-args 1 h/help 'v/version=' -- $argv
     or return
-    if set -q _flag_help; or test (count $argv_opts) -eq 0
+    if set -q _flag_help; or test (count $argv) -eq 0
         echo "\
 Generate valid hash for build vscode extension with nix.
 
 Usage:
-    nix-vscode-extension-hash -a <author> -n <name> -v <version>
-    nix-vscode-extension-hash --author=<author> --name=<name> --version=<version>
+    nix-vscode-extension-hash <publisher.name>
+    nix-vscode-extension-hash <publisher.name> [-v <version>]
     nix-vscode-extension-hash -h | --help"
         return 0
     end
 
-    if not set -ql _flag_author _flag_name _flag_version
-        echo "error: required options not set" >&2
+    set -l ext_id $argv[1]
+    if test -z $ext_id
+        echo "error: missing extension id argument" >&2
         return 1
     end
 
+    set -l ext_id_parts (string split -m1 -n '.' $ext_id)
+    if test (count $ext_id_parts) -ne 2
+        echo "error: expected identifier with format 'publisher.name', got: '$ext_id'" >&2
+        return 1
+    end
+
+    set -l ext_publisher $ext_id_parts[1]
+    set -l ext_name $ext_id_parts[2]
+
+    set -l ext_version latest
+    if set -q _flag_version
+        set ext_version $_flag_version
+    end
+
+    if [ "$ext_version" = latest ]
+        set -l body (jq -n --arg id "$ext_publisher.$ext_name" \
+            '{filters:[{criteria:[{filterType:7,value:$id}],pageNumber:1,pageSize:1}],flags:512}')
+
+        set ext_version (
+            curl -fsSL \
+                -H "Content-Type: application/json" \
+                -H "Accept: application/json;api-version=6.0-preview.1" \
+                --data "$body" \
+                "https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery" | \
+            jq -er '.results.[0].extensions[0].versions[0].version')
+    end
+
     set -l hash (nix-prefetch-url --type sha256 \
-        "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$_flag_author/vsextensions/$_flag_name/$_flag_version/vspackage")
+        "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$ext_publisher/vsextensions/$ext_name/$ext_version/vspackage")
     set hash (echo $hash | tail -1)
     set -l hash64 (nix-hash --type sha256 --to-base64 "$hash")
-    echo "sha256-$hash64"
+
+    echo "\
+mktplcRef = {
+    name = \"$ext_name\";
+    publisher = \"$ext_publisher\";
+    version = \"$ext_version\";
+    hash = \"sha256-$hash64\";
+}"
 end
