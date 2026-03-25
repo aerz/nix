a5c81a0ff800e09395e1a3718309cf3237396891
Agustín Cisneros <agustincc@tutanota.com>
2026-03-25 10:04:02 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    determinate.url = "github:DeterminateSystems/determinate/v3.17.0";
+    determinate.url = "github:DeterminateSystems/determinate/v3.17.1";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
