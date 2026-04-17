6702ae0d854ecc3e0fa2f555ac7fb2dc95b7e36a
Agustín Cisneros <agustincc@tutanota.com>
2026-04-17 13:09:27 +0200

    move nix-hombrew to brew.nix for cohesion

--- hosts/alterac/brew.nix
+++ hosts/alterac/brew.nix
@@ -1,4 +1,39 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  nix-homebrew = {
+    enable = true;
+    autoMigrate = true;
+    mutableTaps = false;
+    enableRosetta = false;
+    user = "aerz";
+
+    # run the following command to add a tap
+    # nix-prefetch-github homebrew homebrew-core --nix
+    taps = {
+      # homebrew-core and homebrew-cask are not pinned here
+      # since homebrew 4.0 formulas resolve via JSON API not local git clone
+      # pinning core re-enables legacy code path which overrides third-party
+      # taps on upgrade
+      # "homebrew/homebrew-core" = ...;
+      # "homebrew/homebrew-cask" = ...;
+      "tw93/homebrew-tap" = pkgs.fetchFromGitHub {
+        owner = "tw93";
+        repo = "homebrew-tap";
+        rev = "0adc601db156eb25901c16410d98383a13fbfd82";
+        hash = "sha256-IT/rzv/CVtOoYrEfTQXq5BET8obwfW5bLYoO2yhTadM=";
+      };
+      "anomalyco/homebrew-tap" = pkgs.fetchFromGitHub {
+        owner = "anomalyco";
+        repo = "homebrew-tap";
+        rev = "7c338f62d34e0bea26d3e8ebee55535ee612c253";
+        hash = "sha256-RXqBoRogRSGAZs9saCc1Zk8NkmJiI9PmRTYR29AObnQ=";
+      };
+    };
+  };
+
   homebrew = {
     enable = true;
 
--- hosts/alterac/default.nix
+++ hosts/alterac/default.nix
@@ -1,9 +1,4 @@
-{
-  self,
-  inputs,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   nixpkgs.hostPlatform = "aarch64-darwin";
 
   imports = [
@@ -32,37 +27,6 @@
   # https://github.com/nix-darwin/nix-darwin/issues/1237
   users.users.aerz.shell = pkgs.fish;
   programs.fish.enable = true;
-
-  nix-homebrew = {
-    enable = true;
-    autoMigrate = true;
-    mutableTaps = false;
-    enableRosetta = false;
-    user = "aerz";
-
-    # run the following command to add a tap
-    # nix-prefetch-github homebrew homebrew-core --nix
-    taps = {
-      # homebrew-core and homebrew-cask are not pinned here
-      # since homebrew 4.0 formulas resolve via JSON API not local git clone
-      # pinning core re-enables legacy code path which overrides third-party
-      # taps on upgrade
-      # "homebrew/homebrew-core" = ...;
-      # "homebrew/homebrew-cask" = ...;
-      "tw93/homebrew-tap" = pkgs.fetchFromGitHub {
-        owner = "tw93";
-        repo = "homebrew-tap";
-        rev = "0adc601db156eb25901c16410d98383a13fbfd82";
-        hash = "sha256-IT/rzv/CVtOoYrEfTQXq5BET8obwfW5bLYoO2yhTadM=";
-      };
-      "anomalyco/homebrew-tap" = pkgs.fetchFromGitHub {
-        owner = "anomalyco";
-        repo = "homebrew-tap";
-        rev = "7c338f62d34e0bea26d3e8ebee55535ee612c253";
-        hash = "sha256-RXqBoRogRSGAZs9saCc1Zk8NkmJiI9PmRTYR29AObnQ=";
-      };
-    };
-  };
 
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
