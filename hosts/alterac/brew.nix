40bcb21c8d28457325a84495a6ca4497f74ac31b
Agustín Cisneros <agustincc@tutanota.com>
2026-06-05 15:07:21 +0200

    update flake

--- hosts/alterac/brew.nix
+++ hosts/alterac/brew.nix
@@ -16,26 +16,26 @@
       "homebrew/homebrew-core" = pkgs.fetchFromGitHub {
         owner = "homebrew";
         repo = "homebrew-core";
-        rev = "eabc0ee13e7e28e5682fcb0ede4a333bc886ebf4";
-        sha256 = "1n4yfk2gj4mv804va9cvr9nn42mf0vkshp5b62c2viidnfaf83vj";
+        rev = "d33a3c1afb7056c23fd349cb4650a8d40b13ca54";
+        sha256 = "01xfxnrf7lqjdyifng4swrfdw6kc1kzppqphks324dvsaqda4jbf";
       };
       "homebrew/homebrew-cask" = pkgs.fetchFromGitHub {
         owner = "homebrew";
         repo = "homebrew-cask";
-        rev = "f05ae747a0fb5d6f21c3fd9b64406018f5870388";
-        sha256 = "048cimxyqy71b6p67dvq253ixiww4d41mrh99mwk7ipbxq6jay5f";
+        rev = "2392b189ac333c7d8d8fdd4507c13fceb34bc4c3";
+        sha256 = "0ds939q5a736bw3qljpzxdhjmagpmvni49i2ag80pxainain3car";
       };
       "tw93/homebrew-tap" = pkgs.fetchFromGitHub {
         owner = "tw93";
         repo = "homebrew-tap";
-        rev = "2570a9405844c3f45eb8a132afa3cbf5d8a09fbf";
-        sha256 = "059js5701mbjcmc82awnmszpjqwi8cq6yar3rm1pxyxhddmxwmzx";
+        rev = "c440ffcb027c71d135b3a149ef522dbfb29bbcba";
+        sha256 = "17xwyssimxi4ayvlfgw2vww5pxfjrf7j9ym713bzakbsfj4lma39";
       };
       "anomalyco/homebrew-tap" = pkgs.fetchFromGitHub {
         owner = "anomalyco";
         repo = "homebrew-tap";
-        rev = "d81638f1a16fda9b104075be87ad1735029886b1";
-        sha256 = "0nmynpm8hbvw2wpkby80hgjw1gnq0n85sdnhs8pjhiiylzak8md1";
+        rev = "93b5615480f331cabc70b17276759024edbb94a4";
+        sha256 = "0bh8m7x2zm0724c3j5h0wk2lkq8qkz6v1sbj2fd9526mx4b801ym";
       };
     };
   };
